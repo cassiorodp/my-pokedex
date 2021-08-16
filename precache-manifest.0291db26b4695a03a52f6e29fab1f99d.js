2cf43c4811553e905559694a91d6b92e5421f15a
Cássio Pereira <cassiorodp@gmail.com>
2021-08-16 17:53:19 -0300

    Updates

---
+++ precache-manifest.0291db26b4695a03a52f6e29fab1f99d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b523ef51b97c3a9303a71f7892c7dfa9",
+    "url": "/my-pokedex/index.html"
+  },
+  {
+    "revision": "7232edd965ceff5f595c",
+    "url": "/my-pokedex/static/css/main.53297c77.chunk.css"
+  },
+  {
+    "revision": "a4d63c859ca0978effc5",
+    "url": "/my-pokedex/static/js/2.96282933.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-pokedex/static/js/2.96282933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7232edd965ceff5f595c",
+    "url": "/my-pokedex/static/js/main.fbd290ab.chunk.js"
+  },
+  {
+    "revision": "3ce3f06a6982683bd48f",
+    "url": "/my-pokedex/static/js/runtime-main.5beaabc1.js"
+  },
+  {
+    "revision": "66fb627227d3b397ac512671ec68dd45",
+    "url": "/my-pokedex/static/media/EachWellinformedAidi-small.66fb6272.gif"
+  }
+]);
